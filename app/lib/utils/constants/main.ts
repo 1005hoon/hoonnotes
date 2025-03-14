7e934b11343025b8704c3b5d4178c687b7edefa0
Hoon Oh <shoh@softly.ai>
2025-03-15 00:47:30 +0900

    feat: implement metadata constructing functionality

---
+++ app/lib/utils/constants/main.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Hoonnotes";
+
+export const SHORT_DOMAIN =
+  process.env.NEXT_PUBLIC_APP_SHORT_DOMAIN || "hoonnotes.com";
+
+export const HOME_DOMAIN = `https://${process.env.NEXT_PUBLIC_APP_DOMAIN}`;
---
+++ app/lib/utils/functions/construct-metadata.ts
@@ -0,0 +1,93 @@
+import { HOME_DOMAIN } from "@/lib/utils/constants";
+import { Metadata } from "next";
+
+export function constructMetadata({
+  title,
+  fullTitle,
+  description = "Product Engineer, with touch of Marketing and AI",
+  // TODO: update images
+  image = "https://assets.dub.co/thumbnail.jpg",
+  video,
+  icons = [
+    {
+      rel: "apple-touch-icon",
+      sizes: "32x32",
+      url: "https://assets.dub.co/favicons/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "https://assets.dub.co/favicons/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "https://assets.dub.co/favicons/favicon-16x16.png",
+    },
+  ],
+  url,
+  canonicalUrl,
+  noIndex = false,
+  manifest,
+}: {
+  title?: string;
+  fullTitle?: string;
+  description?: string;
+  image?: string | null;
+  video?: string | null;
+  icons?: Metadata["icons"];
+  url?: string;
+  canonicalUrl?: string;
+  noIndex?: boolean;
+  manifest?: string | URL | null;
+} = {}): Metadata {
+  return {
+    title:
+      fullTitle ||
+      (title
+        ? `${title} | Hoonnotes`
+        : "Hoonnotes - Product Engineer, with touch of Marketing and AI"),
+    description,
+    openGraph: {
+      title,
+      description,
+      ...(image && {
+        images: image,
+      }),
+      url,
+      ...(video && {
+        videos: video,
+      }),
+    },
+    twitter: {
+      title,
+      description,
+      ...(image && {
+        card: "summary_large_image",
+        images: [image],
+      }),
+      ...(video && {
+        player: video,
+      }),
+      creator: "@1005hoon",
+    },
+    icons,
+    metadataBase: new URL(HOME_DOMAIN),
+    ...((url || canonicalUrl) && {
+      alternates: {
+        canonical: url || canonicalUrl,
+      },
+    }),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+    ...(manifest && {
+      manifest,
+    }),
+  };
+}
